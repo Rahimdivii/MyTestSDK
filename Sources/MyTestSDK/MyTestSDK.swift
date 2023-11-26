f0ad220adddf9fd725453328a8235b3990dd6172
Rahimdivii <106316919+Rahimdivii@users.noreply.github.com>
2023-11-26 13:18:52 +0530

    randamGenerator code

--- Sources/MyTestSDK/MyTestSDK.swift
+++ Sources/MyTestSDK/MyTestSDK.swift
@@ -1,6 +1,18 @@
-public struct MyTestSDK {
-    public private(set) var text = "Hello, World!"
+//public struct MyTestSDK {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
 
-    public init() {
+import Foundation
+
+
+public class RandamGenerator{
+    public static func generateUUID() -> String{
+        return UUID().uuidString
+    }
+    public static func generateNumber() -> Int {
+       Int(arc4random())
     }
 }
